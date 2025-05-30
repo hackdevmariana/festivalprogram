d35342f4823fbe313d57ee5718456126f7ed1d0d
Fernando <fernando@juandemariana.org>
2025-05-30 23:02:56 +0200

    Add Region Filament Resource.

---
+++ app/Filament/Resources/RegionResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RegionResource\Pages;
+use App\Models\Region;
+use Filament\Forms;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class RegionResource extends Resource
+{
+    protected static ?string $model = Region::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-map';
+
+    public static function form(Forms\Form $form): Forms\Form
+    {
+        return $form->schema([
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+            TextInput::make('slug')
+                ->required()
+                ->maxLength(255)
+                ->unique(Region::class),
+        ]);
+    }
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table->columns([
+            TextColumn::make('id')->sortable(),
+            TextColumn::make('name')->sortable()->searchable(),
+            TextColumn::make('slug')->sortable()->searchable(),
+            TextColumn::make('created_at')->dateTime(),
+        ]);
+        
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRegions::route('/'),
+            'create' => Pages\CreateRegion::route('/create'),
+            'edit' => Pages\EditRegion::route('/{record}/edit'),
+        ];
+    }
+}
