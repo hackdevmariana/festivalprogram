feaefe011c968da11592b11b646047d7dbb7cc1b
Fernando <fernando@juandemariana.org>
2025-05-30 23:15:22 +0200

    Add fillable to Province model.

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Province extends Model
 {
+    protected $fillable = ['name', 'slug', 'region_id'];
+
     public function region()
     {
         return $this->belongsTo(Region::class);
