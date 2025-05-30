72bb40635f7b2e13d85951f82a92cf90cea92522
Fernando <fernando@juandemariana.org>
2025-05-30 23:14:47 +0200

    Add fillable to Region model.

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
+    protected $fillable = ['name', 'slug'];
+
     public function provinces()
     {
         return $this->hasMany(Province::class);
