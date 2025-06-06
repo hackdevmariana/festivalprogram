37d333aef1ae29d757fbbece9bea9a96b488329f
Fernando <fernando@juandemariana.org>
2025-06-06 22:27:57 +0200

    Add AppSetting migration.

--- database/migrations/2025_06_06_202602_create_app_settings_table.php
+++ database/migrations/2025_06_06_202602_create_app_settings_table.php
@@ -1,27 +1,27 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateAppSettingsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('app_settings', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name'); // Nombre de la aplicación
+            $table->string('logo'); // Ruta al logotipo
+            $table->string('slogan'); // Lema de la aplicación
+            $table->string('domain'); // Dominio o URL principal
+            $table->enum('copy_type', ['copyright', 'copyleft']); // Tipo de copyright o copyleft
+            $table->string('developed_by'); // Quién desarrolló la aplicación
+            $table->string('developed_url'); // URL del desarrollador o equipo
+            $table->timestamps(); // Si quieres registrar las fechas de creación y actualización
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('app_settings');
     }
-};
+}
