df9c1126556a9faac825a4c64df969e0561378c0
Fernando <fernando@juandemariana.org>
2025-06-06 22:26:43 +0200

    Add AppSetting model.

---
+++ app/Models/AppSetting.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppSetting extends Model
+{
+    protected $fillable = [
+        'name',
+        'logo',
+        'slogan',
+        'domain',
+        'copy_type', // Puede ser 'copyright' o 'copyleft'
+        'developed_by',
+        'developed_url'
+    ];
+
+    // Si no se usan los campos 'created_at' y 'updated_at', podemos desactivarlos.
+    public $timestamps = false;
+}
