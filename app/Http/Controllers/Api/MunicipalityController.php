c4d26850732db2eb7536bef76110b448644cb07a
Fernando <fernando@juandemariana.org>
2025-06-06 14:02:31 +0200

    Returns the api/v1/municipalities route with only the relevant information.

--- app/Http/Controllers/Api/MunicipalityController.php
+++ app/Http/Controllers/Api/MunicipalityController.php
@@ -9,10 +9,35 @@ use App\Models\Municipality;
 
 class MunicipalityController extends Controller
 {
-    public function indexAll()
-    {
-        return response()->json(Municipality::with(['province.region'])->get());
-    }
+public function indexAll()
+{
+    $municipalities = Municipality::with([
+        'province.region'
+    ])->select('name', 'slug', 'latitude', 'longitude', 'province_id')->get();
+
+    $transformed = $municipalities->map(function ($municipality) {
+        return [
+            'name' => $municipality->name,
+            'slug' => $municipality->slug,
+            'latitude' => $municipality->latitude,
+            'longitude' => $municipality->longitude,
+            'province' => [
+                'name' => $municipality->province->name,
+                'slug' => $municipality->province->slug,
+                'region' => [
+                    'name' => $municipality->province->region->name,
+                    'slug' => $municipality->province->region->slug,
+                    'flag' => 'storage/' . $municipality->province->region->flag,
+                    'button_flag' => 'storage/' . $municipality->province->region->button_flag,
+                ],
+            ],
+        ];
+    });
+
+    return response()->json($transformed);
+}
+
+
 
     public function show(Municipality $municipality)
     {
