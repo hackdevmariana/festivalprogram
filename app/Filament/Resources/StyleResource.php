d257a15f52e5dc4d8cbdfed57340a903e3091fc2
Fernando <fernando@juandemariana.org>
2025-06-06 23:21:48 +0200

    Add Style Filament resource.

---
+++ app/Filament/Resources/StyleResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StyleResource\Pages;
+use App\Models\Style;
+use Filament\Forms;
+use Filament\Resources\Resource;
+use Filament\Tables;
+
+class StyleResource extends Resource
+{
+    protected static ?string $model = Style::class;
+
+    protected static ?string $navigationLabel = 'Estilos';
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Forms\Form $form): Forms\Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(Style::class, 'slug'),
+            ]);
+    }
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->sortable(),
+            ])
+            ->filters([
+                // Agregar filtros si es necesario
+            ])
+            ->actions([
+                // Acciones como "ver", "editar", etc.
+            ])
+            ->bulkActions([
+                // Acciones masivas como "eliminar"
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStyles::route('/'),
+            'create' => Pages\CreateStyle::route('/create'),
+            'edit' => Pages\EditStyle::route('/{record}/edit'),
+        ];
+    }
+}
