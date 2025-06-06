1a1e7ed2d47c86b4cb56e0163b1c2295ff9970c8
Fernando <fernando@juandemariana.org>
2025-06-06 23:44:06 +0200

    Add Artist routes.

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\Api\RegionController;
 use App\Http\Controllers\Api\ProvinceController;
 use App\Http\Controllers\Api\MunicipalityController;
 use App\Http\Controllers\Api\StyleController;
-
+use App\Http\Controllers\Api\ArtistController;
 
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
     // Locations
-    
+
     Route::get('/regions', [RegionController::class, 'index']); // Lista todas las regiones
     Route::get('/regions/provinces', [RegionController::class, 'regionsWithProvinces']); // Todas las regiones con sus provincias
     Route::get('/region/{slug}/provinces', [RegionController::class, 'provincesByRegionSlug']); // Provincias de una región
@@ -28,4 +28,9 @@ Route::prefix('v1')->group(function () {
     Route::put('/style/{style}', [StyleController::class, 'update']); // Actualizar un estilo
     Route::delete('/style/{style}', [StyleController::class, 'destroy']); // Eliminar un estilo
 
+
+    // Artist
+    
+    Route::get('/artists', [ArtistController::class, 'index']);  // Obtener todos los artistas
+    Route::get('/artist/{slug}', [ArtistController::class, 'show']);  // Obtener un artista por slug
 });
