2198d1b061427f4a51affca9f1f24d50977ea46b
Fernando <fernando@juandemariana.org>
2025-06-06 23:20:41 +0200

    Add Styles routes.

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,15 @@
 use App\Http\Controllers\Api\RegionController;
 use App\Http\Controllers\Api\ProvinceController;
 use App\Http\Controllers\Api\MunicipalityController;
+use App\Http\Controllers\Api\StyleController;
+
 
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+
+    // Locations
+    
     Route::get('/regions', [RegionController::class, 'index']); // Lista todas las regiones
     Route::get('/regions/provinces', [RegionController::class, 'regionsWithProvinces']); // Todas las regiones con sus provincias
     Route::get('/region/{slug}/provinces', [RegionController::class, 'provincesByRegionSlug']); // Provincias de una región
@@ -14,4 +19,13 @@ Route::prefix('v1')->group(function () {
     Route::get('/municipalities', [MunicipalityController::class, 'indexAll']); // Todas las regiones con provincias y municipios
     Route::get('/region/{slug}/municipalities', [RegionController::class, 'municipalitiesByRegion']); // Municipios de una región
     Route::get('/municipality/{municipality}', [MunicipalityController::class, 'show']); // Detalle de un municipio
+
+    // Styles
+
+    Route::get('/styles', [StyleController::class, 'index']); // Obtener todos los estilos
+    Route::get('/style/{style}', [StyleController::class, 'show']); // Obtener un estilo por slug
+    Route::post('/styles', [StyleController::class, 'store']); // Crear un nuevo estilo
+    Route::put('/style/{style}', [StyleController::class, 'update']); // Actualizar un estilo
+    Route::delete('/style/{style}', [StyleController::class, 'destroy']); // Eliminar un estilo
+
 });
