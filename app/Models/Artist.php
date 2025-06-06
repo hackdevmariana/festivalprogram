e6fef39e9a71bbbe839cc5eca44bda00cc16815c
Fernando <fernando@juandemariana.org>
2025-06-06 23:41:44 +0200

    Add Artist model.

---
+++ app/Models/Artist.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str; 
+
+class Artist extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'bio', 'photo'];
+
+    // Generar el slug automáticamente
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($artist) {
+            $artist->slug = Str::slug($artist->name);
+        });
+    }
+}
