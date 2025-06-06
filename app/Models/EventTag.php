17be1f5c8fe53a8361d26fad8ec82e69413bb573
Fernando <fernando@juandemariana.org>
2025-06-07 00:32:57 +0200

    Fix EventTag model.

--- app/Models/EventTag.php
+++ app/Models/EventTag.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class EventTag extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
 }
