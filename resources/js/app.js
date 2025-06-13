766f4ac351e0017e7c9d986c6d1ac032b4f56f9a
Fernando <fernando@juandemariana.org>
2025-06-13 23:12:33 +0200

    Config fullcalendar to project.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,29 @@
 import './bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+document.addEventListener('DOMContentLoaded', function () {
+    let calendarEl = document.getElementById('calendar');
+
+    let calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        selectable: true,
+        dateClick: function(info) {
+            fetch(`/api/events/${info.dateStr}`)
+                .then(response => response.json())
+                .then(data => {
+                    // Muestra los eventos de ese día como quieras (modal, lista, etc.)
+                    console.log(data);
+                });
+        },
+        events: '/api/events', // Carga eventos desde backend
+    });
+
+    calendar.render();
+});
