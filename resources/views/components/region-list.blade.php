11573578d5da46304bf7f816fec189241522888e
Fernando <fernando@juandemariana.org>
2025-06-15 23:57:08 +0200

    Fix region-list Blade component to show button_flag.

--- resources/views/components/region-list.blade.php
+++ resources/views/components/region-list.blade.php
@@ -5,8 +5,8 @@
                 <a href="{{ route('regions.show', $region->slug) }}" class="text-decoration-none">
                     <h3>{{ $region->name }}</h3>
                 </a>
-                <a href="{{ route('regions.show', $region->slug) }}" class="btn btn-primary">
-                    <img src="{{ asset('images/' . $region->button_flag) }}" alt="{{ $region->name }}" width="30">
+                <a href="{{ route('regions.show', $region->slug) }}" >
+                    <img src="{{ asset('storage/' . $region->button_flag) }}" alt="{{ $region->name }}" width="30">
                 </a>
             </div>
         @endforeach
