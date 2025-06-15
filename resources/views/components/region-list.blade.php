a492a4561db1ee85ed1c047f5f077e0014af5c47
Fernando <fernando@juandemariana.org>
2025-06-15 23:59:57 +0200

    Fix region-list.

--- resources/views/components/region-list.blade.php
+++ resources/views/components/region-list.blade.php
@@ -1,14 +1,17 @@
 <div class="container mt-4">
     <div class="row">
         @foreach ($regions as $region)
-            <div class="col-md-4 text-center mb-3">
-                <a href="{{ route('regions.show', $region->slug) }}" class="text-decoration-none">
-                    <h3>{{ $region->name }}</h3>
-                </a>
-                <a href="{{ route('regions.show', $region->slug) }}" >
-                    <img src="{{ asset('storage/' . $region->button_flag) }}" alt="{{ $region->name }}" width="30">
-                </a>
+            <div class="col-md-4 mb-3">
+                <div class="d-flex align-items-center">
+                    <a href="{{ route('regions.show', $region->slug) }}" class="btn btn-primary me-2">
+                        <img src="{{ asset('storage/' . $region->button_flag) }}" alt="{{ $region->name }}" width="30">
+                    </a>
+                    <a href="{{ route('regions.show', $region->slug) }}" class="text-decoration-none">
+                        <h3 class="mb-0">{{ $region->name }}</h3>
+                    </a>
+                </div>
             </div>
         @endforeach
     </div>
 </div>
+
