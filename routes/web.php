3f40f92f4c086bdc4c12c4429c46ff86e5402802
Fernando <fernando@juandemariana.org>
2025-06-15 23:54:59 +0200

    Add /region/{slug} route.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegionController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/region/{slug}', [RegionController::class, 'show'])->name('regions.show');
