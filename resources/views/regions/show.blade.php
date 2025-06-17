456dfe8bcecf35456f15a28d68ee888dcffc9250
Fernando <fernando@juandemariana.org>
2025-06-17 23:56:11 +0200

    Positions the name of the municipalities in columns from all display.

--- resources/views/regions/show.blade.php
+++ resources/views/regions/show.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <h2>{{ $region->name }}</h2>
 
         <div class="row">
             @foreach ($region->provinces as $province)
                 <div class="col-md-4 mb-3">
                     <div class="province-card p-3 border rounded">
-                        <h4 class="province-name" data-target="municipalities-{{ $province->id }}">{{ $province->name }}</h4>
+                        <h4 class="province-name" data-target="municipalities-{{ $province->id }}">
+                            {{ $province->name }}
+                        </h4>
+                    </div>
+                </div>
 
-                        <div id="municipalities-{{ $province->id }}" class="municipality-list mt-2" style="display: none;">
-                            <div class="row">
-                                @foreach ($province->municipalities as $municipality)
-                                    <div class="col-12 col-md-4 col-xl-2 mb-2">
-                                        <div class="bg-light p-2 border rounded text-center shadow-sm hover-shadow">
-                                            {{ $municipality->name }}
-                                        </div>
-
+                {{-- Bloque de municipios a pantalla completa --}}
+                <div id="municipalities-{{ $province->id }}" class="municipality-list mb-4 w-100" style="display: none;">
+                    <div class="container-fluid">
+                        <div class="row">
+                            @foreach ($province->municipalities as $municipality)
+                                <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-2">
+                                    <div class="bg-light p-2 border rounded text-center shadow-sm hover-shadow">
+                                        {{ $municipality->name }}
                                     </div>
-                                @endforeach
-                            </div>
-
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
