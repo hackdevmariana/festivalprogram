5603d5b1967e77fd4b656fbf6c85703e63f15383
Fernando <fernando@juandemariana.org>
2025-06-17 23:48:00 +0200

    Positions the name of the municipalities in columns.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Mi Aplicación')</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
     <nav>
--- resources/views/regions/show.blade.php
+++ resources/views/regions/show.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2>{{ $region->name }}</h2>
-    
-    <div class="row">
-        @foreach ($region->provinces as $province)
-            <div class="col-md-4 mb-3">
-                <div class="province-card p-3 border rounded">
-                    <h4 class="province-name" data-target="municipalities-{{ $province->id }}">{{ $province->name }}</h4>
-                    
-                    <div id="municipalities-{{ $province->id }}" class="municipality-list mt-2" style="display: none;">
-                        @foreach ($province->municipalities as $municipality)
-                            <p>{{ $municipality->name }}</p>
-                        @endforeach
+    <div class="container">
+        <h2>{{ $region->name }}</h2>
+
+        <div class="row">
+            @foreach ($region->provinces as $province)
+                <div class="col-md-4 mb-3">
+                    <div class="province-card p-3 border rounded">
+                        <h4 class="province-name" data-target="municipalities-{{ $province->id }}">{{ $province->name }}</h4>
+
+                        <div id="municipalities-{{ $province->id }}" class="municipality-list mt-2" style="display: none;">
+                            <div class="row">
+                                @foreach ($province->municipalities as $municipality)
+                                    <div class="col-12 col-md-4 col-xl-2 mb-2">
+                                        <div class="bg-light p-2 border rounded text-center shadow-sm hover-shadow">
+                                            {{ $municipality->name }}
+                                        </div>
+
+                                    </div>
+                                @endforeach
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
 @endsection
